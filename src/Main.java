f23f1dcad4077f4658948a3837f765cf2db8f027
Aleksey Koshelev <aleksey.koshelev@skyeng.ru>
2024-04-13 14:21:32 +0400

    Homework is done!

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("World!");
         System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello!");
+        System.out.println("Hello");
     }
 }
